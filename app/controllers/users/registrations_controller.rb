49e56bdcd9d29fbb6866f85feaedf36c97020c6d
duleep <duleep.daniel@gmail.com>
2020-06-29 15:08:22 +0530

    dynamic role adding

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  #before_action :configure_sign_up_params, only: [:create]
-  #def create
-  #  super
-  #  resource.add_role(params[:user][:roles])
-  #end
+  before_action :configure_sign_up_params, only: [:create]
+  def create
+    super
+    resource.add_role(params[:user][:roles])
+  end
 
-  #private
-  #def configure_sign_up_params
-  #  devise_parameter_sanitizer.permit(:sign_up, keys: [roles: []])
-  #end
+  protected
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [roles: []])
+  end
   
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
