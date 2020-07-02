e3317900eb858e09d5f993f6ed492776a32f610f
duleep <duleep.daniel@gmail.com>
2020-07-02 12:13:57 +0530

    admin adding meetings v2

--- app/assets/javascripts/meetings.js
+++ app/assets/javascripts/meetings.js
@@ -1,16 +1,13 @@
-(function() {
-  jQuery(function() {
-    var patients;
+$(document).ready(function(){
+  var patients;
     patients = $('#meeting_patient_id').html();
-    console.log(patients)
 
     $('#meeting_user_id').change(function() {
-      var user, new_patients;
-      user = $('meeting_user_email :selected');
-      new_patients = $(patients).filter("optgroup[label='" + user + "']").html();
-      console.log(new_patients)
+      var selected_user, new_patients, users;
+
+      users = $('#meeting_user_id').html();
+      selected_user = $('#meeting_user_id option:selected');
+      new_patients = $(patients).filter("optgroup[label='" + selected_user.text() + "']").html();
       return $('#meeting_patient_id').html(new_patients);
     });
   });
-
-}).call(this);
